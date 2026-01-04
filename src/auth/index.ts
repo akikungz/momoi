61d2c97c9e7908cd958bcdc925ed9f14e9aa4528
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2026-01-04 17:29:37 +0700

    fix: Update authHandler mount path to allow all routes

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -148,7 +148,7 @@ export const authOpenAPI = async (_auth: typeof auth = auth) => {
 }
 
 export const authHandler = new Elysia({ name: "auth.handler", prefix: "/auth" })
-  .mount("/*", auth.handler);
+  .mount("*", auth.handler);
 
 export const authMacro = new Elysia({ name: "auth.macro" })
   .decorate("cache", new CacheModule())
